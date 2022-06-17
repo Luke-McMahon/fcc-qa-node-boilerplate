ae62f56060c2e6efac0f7d7c0155d0bedf83e893
Luke McMahon <luke.mcmahon@twobulls.com>
2022-06-17 10:34:29 +1000

    feat: include session and passport

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 "use strict";
 require("dotenv").config();
 const express = require("express");
+const passport = require("passport");
+const session = require("express-session");
 const myDB = require("./connection");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
 
 const app = express();
-
 fccTesting(app); //For FCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+app.use(passport.initialize());
 
 app.set("view engine", "pug");
 
